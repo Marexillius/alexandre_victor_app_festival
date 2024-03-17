c9161b5117b0fcec5632603911241b5668122b02
Marexillius <alexandregervaisme@gmail.com>
2024-03-17 16:51:18 -0400

    1.3c fini (mais bug swiper)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
-const filterSwipe = document.querySelector(".swiper-filters");
-const cardSwipe = document.querySelector(".swiper-featured-artworks");
-if (filterSwipe) {
-    var swiper1 = new Swiper(".swiper-filters", {
-        direction: "horizontal",
-        spaceBetween: 10,
-        effect: "slide",
-        freeMode: true,
-    });
-}
+let heartContainer = document.querySelector(".heart-container");
+let heart = document.querySelector(".heart-icon");
+heartContainer.addEventListener("click", function(){
+    if (heart.classList.contains("bi-heart-fill")){
+        heart.classList.add("bi-heart");
+        heart.classList.remove("bi-heart-fill");
+    } else if (heart.classList.contains("bi-heart")){
+        heart.classList.add("bi-heart-fill");
+        heart.classList.remove("bi-heart");
+    }
+});
 
-if (cardSwipe) {
-    var swiper2 = new Swiper(".swiper-featured-artworks", {
-        direction: "horizontal",
-        spaceBetween: 10,
-        effect: "slide",
-        freeMode: true,
-    });
-}
+// NOTE: Ce code retourne une erreur que, après 7h de recherches et de demander si quelqu'un d'autre à eu cette erreur, j'ai abandonné. -Alexandre
+var swiper = new Swiper(".swiper-filters", {
+    direction: "horizontal",
+    spaceBetween: 10,
+    effect: "slide",
+    freeMode: true,
+});
+
+
+
+var swiper2 = new Swiper(".swiper-featured-artworks", {
+    direction: "horizontal",
+    spaceBetween: 10,
+    effect: "slide",
+    freeMode: true,
+});
